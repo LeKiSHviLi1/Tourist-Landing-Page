3a6d8f1d5786fa2bd650cf864899de277b935034
LeKiSHviLi1 <58632301+LeKiSHviLi1@users.noreply.github.com>
2022-11-18 16:17:47 +0400

    add burger menu

--- js/app.js
+++ js/app.js
@@ -64,10 +64,17 @@ function card() {
 
 card();
 
-function menuClick() {
-  let div = document.createElement('div');
-  let nav = document.createElement('nav');
-  for (let i = 0; i < 4; i++) {
-    let a = document.createElement('a');
-  }
-}
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav--menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav--link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  })
+);
